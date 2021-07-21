d1cdc6b5a2b7ee206f58476375a8aabdd4e2133d
Anmol <anmolmangla1@gmail.com>
2021-07-22 00:17:00 +0530

    creating views and connecting to controller

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //using express router
 app.use('/', require('./routes/index.js'));
 
+//set up views engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 
 app.listen(port, function(err){
