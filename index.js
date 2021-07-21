c6e81764d6685d52fcdee02ee24d5872a10f87b0
Anmol <anmolmangla1@gmail.com>
2021-07-22 01:12:52 +0530

    setting up assets

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const app = express();
 const port = 8000;
 const expressLayout = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 app.use(expressLayout);
+
+//for extracting  style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 //using express router
 app.use('/', require('./routes/index.js'));
