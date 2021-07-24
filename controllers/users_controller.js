38d0dabc834beb5b9158148485b6bcce185ad723
Anmol <anmolmangla1@gmail.com>
2021-07-25 00:13:40 +0530

    profile, signin, cookies

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,21 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req, res){
-    return res.render('user_profile',{
-        title : "User Profile"
-    });
+
+    if(req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if(user){
+                return res.render('user_profile',{
+                    title : "User Profile",
+                    user: user
+                });
+            }
+
+            return res.redirect('/users/sign-in');
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+    }
 }
 
 module.exports.signUp = function(req, res){
@@ -24,23 +36,36 @@ module.exports.create = function(req, res){
     }
 
     User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('Error in finding user in signing up'); return}
+        if(err){console.log('User already exist'); return}
     
     
-    if(!user){
-        User.create(req.body, function(err, user){
-            if(err){console.log('Error in finding user in signing up'); return}
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error'); return}
 
-            return res.redirect('/users/sign-in');
-        });
-    }else{
-        return res.redirect('back');
-    }
+                return res.redirect('/users/sign-in');
+            });
+        }else{
+            return res.redirect('back');
+        }
 
-});
-
+    });
 }
 
 module.exports.createSession = function(req, res){
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing in'); return}
 
+        if(user){
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            return res.redirect('back');
+        }
+    });   
 }
