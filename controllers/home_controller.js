54b298033d8925d665b252abd741171ccb2ec66f
Anmol <anmolmangla1@gmail.com>
2022-06-10 16:35:35 +0530

    coverting to AJAX(jquery)so page doesnt refresh

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -6,6 +6,7 @@ module.exports.home =  async function(req, res){
     try{
         //populat the user of each object
         let posts = await Post.find({})
+        .sort('-createdAt')
         .populate('user')
         .populate({
             path: 'comments',
